9301ac9c1011c9c74261c51e67521c4db61eac8a
Jesse Wattenbarger <jwatt@broken.watch>
2026-05-19 11:07:34 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -9,12 +9,12 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; }
+        pkgs = import nixpkgs { inherit system; };
       in
         {
           devShells.default = pkgs.mkShell {
             buildInputs = with pkgs; [
-              fennel
+              luaPackages.fennel
               lua
               love
             ];
