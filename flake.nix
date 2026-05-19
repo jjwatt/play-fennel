48e7d3ec6f6e46dd1e15f675de7a41b769d80e1b
Jesse Wattenbarger <jwatt@broken.watch>
2026-05-19 11:06:36 -0400

    Add first shot at a flake.

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A multi-program Fennel & LOVE2D monorepo environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; }
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              fennel
+              lua
+              love
+            ];
+
+            shellHook = ''
+            FENNEL_SHARE="${pkgs.fennel}/share/lua/${pkgs.lua.luaversion}"
+            export LUA_PATH="$FENNEL_SHARE/?.lua;$FENNEL_SHARE/?/init.lua;./?.lua;;"
+
+            echo "Global 'fennel' module is now available to Lua and LOVE2D."
+            '';
+          };
+        });
+}
